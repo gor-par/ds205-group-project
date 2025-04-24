1d93120126355e9574af284882af9623953a0307
gor-par <gor.parisakoyan@gail.com>
2025-04-25 00:54:15 +0400

    Update employee table

--- ddl.sql
+++ ddl.sql
@@ -13,7 +13,6 @@ drop table if exists payment;
 drop table if exists feedback;
 drop table if exists reservation;
 
-drop table if exists employee;
 drop table if exists user_authentication;
 drop table if exists "user";
 
@@ -37,19 +36,6 @@ create table user_authentication (
     security_question_hash text
 );
 
-drop type if exists employee_role;
-create type employee_role as enum('branch_manager', 'cleaner', 'support_agent', 'front_desk_agent', 'lobby_attendant', 'security_guard');
-
-create table employee (
-    employee_id serial primary key,
-    first_name varchar(50) not null,
-    middle_name varchar(50),
-    last_name varchar(50) not null,
-    email varchar(100) unique not null,
-    phone_number varchar(20) unique,
-    role employee_role not null
-);
-
 drop type if exists address;
 
 create type address as (
@@ -65,13 +51,17 @@ create table hotel ( -- same as branch I guess
     hotel_address address,
     room_quantity int not null,
     capacity int not null,
-    branch_manager_id int references employee(employee_id)
+    branch_manager_id int references "user"(user_id)
 );
 
+drop type if exists employee_role;
+create type employee_role as enum('branch_manager', 'cleaner', 'support_agent', 'front_desk_agent', 'lobby_attendant', 'security_guard');
+
 create table employee_details (
-    employee_id int primary key references employee(employee_id),
+    user_id int primary key references "user"(user_id),
+    role employee_role not null,
     contract_start date not null,
-    supervisor_id int references employee(employee_id),
+    supervisor_id int references "user"(user_id),
     salary decimal(10, 2) not null,
     salary_transaction_account text,
     hotel_id int references hotel(hotel_id)
